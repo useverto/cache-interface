e41556afb0e6bef2107584acbed0a0bcc1e02d91
andreespirela <andreespirela@outlook.com>
2022-05-05 17:36:20 -0400

    Fixing hooks

--- src/calls/cache-api-base-request.ts
+++ src/calls/cache-api-base-request.ts
@@ -5,10 +5,11 @@ import {CacheInterfaceConstants} from "../constants";
  * Executes a GET request to the cache API server
  * @param subUrl
  * @param doThrow throw error if request fails
+ * @param post whether it's a post request
  */
-export const cacheApiBaseRequest = async<T= any>(subUrl: string, doThrow?: boolean) => {
+export const cacheApiBaseRequest = async<T= any>(subUrl: string, doThrow?: boolean, post?: boolean) => {
     try {
-        return await axios.get<T>(`${CacheInterfaceConstants.CACHE_API}/${subUrl}`);
+        return await axios[post ? "post" : "get"]<T>(`${CacheInterfaceConstants.CACHE_API}/${subUrl}`);
     } catch (e) {
         if(doThrow) {
             throw e;
